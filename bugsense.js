2542af8a5f1f0205070bc23e073f1a33e7a4d3f2
Luis Abreu <lmjabreu@gmail.com>
2012-11-08 14:54:09 +0000

    removed AMD module definition.. this lib is intentded to be loaded separate from the main app, etc

--- bugsense.js
+++ bugsense.js
@@ -1,18 +1,20 @@
 (function ( root, factory ) {
-    if ( typeof define === 'function' && define.amd ) {
-        // AMD. Register as an anonymous module.
-        define(function () {
-            // Also create a global in case some scripts
-            // that are loaded still are looking for
-            // a global even when an AMD loader is in use.
-            return ( root.Bugsense = factory() );
-        });
-    } else {
-        // Browser globals
-        root.Bugsense = factory();
-        // use the initializer defined in root
-        if ( root.bugsenseInit ) { root.bugsenseInit(); }
-    }
+
+    // if ( typeof define === 'function' && define.amd ) {
+    //   // AMD. Register as an anonymous module.
+    //   define(function () {
+    //       // Also create a global in case some scripts
+    //       // that are loaded still are looking for
+    //       // a global even when an AMD loader is in use.
+    //       return ( root.Bugsense = factory() );
+    //   });
+    // } else {
+      // Browser globals
+      root.Bugsense = factory();
+    // }
+
+    // use the initializer defined in root
+    if ( root.bugsenseInit ) { root.bugsenseInit(); }
 }( this, function () {
 
   /**
